4f53ebcf941b5c932249a78e487fef99ad0cb6e1
Pablo <pablo.mg@hotmail.com>
2020-07-05 22:17:54 -0300

    Added documentation with swagger

---
+++ src/main/java/com/simple/api/foods/config/SwaggerConfig.java
@@ -0,0 +1,44 @@
+package com.simple.api.foods.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.simple.api.foods"))
+                .paths(PathSelectors.regex("/food.*"))
+                .build()
+                .apiInfo(metaInfo());
+    }
+
+    private ApiInfo metaInfo() {
+
+        ApiInfo apiInfo = new ApiInfo(
+                "Foods API REST",
+                "API REST Foods.",
+                "1.0",
+                "Terms of Service",
+                new Contact("Pablo", "",
+                        ""),
+                "Apache License Version 2.0",
+                "https://www.apache.org/licesen.html", new ArrayList<VendorExtension>()
+        );
+
+        return apiInfo;
+    }
+}
--- src/main/java/com/simple/api/foods/resources/FoodResource.java
+++ src/main/java/com/simple/api/foods/resources/FoodResource.java
@@ -3,17 +3,12 @@ package com.simple.api.foods.resources;
 import java.util.HashMap;
 import java.util.List;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.simple.api.foods.entities.Food;
 import com.simple.api.foods.repositories.FoodRepository;
@@ -21,6 +16,8 @@ import com.simple.api.foods.services.FoodService;
 
 @RestController
 @RequestMapping(value = "/food")
+@Api(value = "API REST Foods")
+@CrossOrigin(origins = "*")
 public class FoodResource {
 	
 	@Autowired
@@ -34,42 +31,49 @@ public class FoodResource {
 	}
 	
 	@GetMapping("/allFoods")
+	@ApiOperation(value = "Return list foods")
 	public ResponseEntity<List<Food>> findAll(){
 		List<Food> list = foodService.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 	
 	@GetMapping(value = "findById/{id}")
+	@ApiOperation(value = "Return the food as the id passed")
 	public ResponseEntity<Food> findById(@PathVariable Integer id){
 		Food obj = foodService.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
 	
 	@GetMapping(value = "findByName/{name}")
+	@ApiOperation(value = "Return the food as the name passed")
 	public ResponseEntity<Food> findByName(@PathVariable String name){
 		Food obj = foodService.findByName(name);
 		return ResponseEntity.ok().body(obj);
 	}
 	
 	@PostMapping("/addFood")
+	@ApiOperation(value = "Add food")
 	public ResponseEntity<Food> addFood(@RequestBody Food food) {
 		Food obj = foodService.saveFood(food);
 		return ResponseEntity.ok().body(obj);
 	}
 	
 	@PostMapping("/addFoods")
+	@ApiOperation(value = "Add list of food")
 	public ResponseEntity<List<Food>> addFoods(@RequestBody List<Food> listFood){
 		List<Food> obj = foodService.saveFoods(listFood);
 		return ResponseEntity.ok().body(obj);
 	}
 	
 	@PutMapping("/updateFood")
+	@ApiOperation(value = "Update food")
 	public ResponseEntity<Food> updateFood(@RequestBody Food food) {
 		Food obj = foodService.updateFood(food);
 		return ResponseEntity.ok().body(obj);
 	}
 	
-	@DeleteMapping("/delteFood/{id}")
+	@DeleteMapping("/deleteFood/{id}")
+	@ApiOperation(value = "Delete the food as the id passed")
 	public ResponseEntity<String> deleteFood(@PathVariable Integer id) {
 		String obj = foodService.deleteFoodById(id);
 		return ResponseEntity.ok().body(obj);
